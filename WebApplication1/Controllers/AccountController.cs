80be860970eaceb78c93f0ae377034e2553de9fd
Bugra Kırman <bugrakirman@gmail.com>
2019-02-18 14:59:26 +0300

    register post yarım kaldı

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using TeknikServis.Models.IdentityModels;
+using TeknikServis.Models.ViewModels;
+using static TeknikServis.BLL.Identity.MembershipTools;
 
 namespace WebApplication1.Controllers
 {
@@ -20,10 +23,29 @@ namespace WebApplication1.Controllers
             ViewBag.CountryList = CountryList();
             return View();
         }
-        [HttpPost]
-        public ActionResult Register(User model)
-        {
-            return View();
-        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+
+        //public async Task<ActionResult> Register(RegisterViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+
+        //        return View("Index",model);
+        //    }
+
+        //    var userManager = NewUserManager();
+        //    var roleManager = NewRoleManager();
+
+        //    var rm = RegisterViewModel;
+
+        //    var user = await userManager.FindByNameAsync(rm.UserName);
+        //    if (user != null)
+        //    {
+        //        ModelState.AddModelError("UserName", "daha önce alınmış");
+        //        return View("Index", model);
+        //    }
+        //}
     }
 }
