a52ff22efcc429a5466a4733a2edbb044b8cb9b2
Bugra Kırman <bugrakirman@gmail.com>
2019-02-19 15:03:38 +0300

    partial not okay

---
+++ WebApplication1/Controllers/PartialController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeknikServis.Models.IdentityModels;
+using static TeknikServis.BLL.Identity.MembershipTools;
+
+namespace WebApplication1.Controllers
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult ProfileIndex()
+        {
+            var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+            User user = NewUserManager().FindById(id);
+
+            return PartialView("Partials/_PartialProfile", user);
+        }
+    }
+}
