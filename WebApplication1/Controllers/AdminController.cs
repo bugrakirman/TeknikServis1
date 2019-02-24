8855a6926ca7ca085c9d9e38009561e1a6c9f7a1
Bugra KIRMAN <bugrakirman@gmail.com>
2019-02-25 01:17:59 +0300

    .

--- WebApplication1/Controllers/AdminController.cs
+++ WebApplication1/Controllers/AdminController.cs
@@ -29,5 +29,59 @@ namespace WebApplication1.Controllers
             ViewBag.Roles = roles;
             return View(users);
         }
+
+        [HttpGet]
+        public ActionResult EditModal(string id)
+        {
+            ViewBag.CountryList = CountryList();
+            
+            if (id == null)
+            {
+                User user1 = new User()
+                {
+                    Adress = "",
+                    BirthDate = DateTime.Now,
+                    City = "",
+                    Country = "",
+                    Email = "",
+                    Gender = "",
+                    Name = "",
+                    PhoneNumber = "",
+                    UserName = ""
+                };
+
+                var newUser1 = new RegisterViewModel()
+                {
+                    Adress = user1.Adress,
+                    BirthDate = user1.BirthDate.Date,
+                    City = user1.City,
+                    Country = user1.Country,
+                    Email = user1.Email,
+                    Gender = user1.Gender,
+                    Name = user1.Name,
+                    Surname = user1.Surname,
+                    PhoneNumber = user1.PhoneNumber,
+                    UserName = user1.UserName
+                };
+                return PartialView("Partials/_PartialEditModal", newUser1);
+            }
+            var user = NewUserManager().FindById(id);
+            var newUser = new RegisterViewModel()
+            {
+                Adress = user.Adress,
+                BirthDate = user.BirthDate.Date,
+                City = user.City,
+                Country = user.Country,
+                Email = user.Email,
+                Gender = user.Gender,
+                Name = user.Name,
+                Surname = user.Surname,
+                PhoneNumber = user.PhoneNumber,
+                UserName = user.UserName
+                
+            };
+            return PartialView("Partials/_PartialEditModal", newUser);
+        }
     }
-}
+} 
+    
